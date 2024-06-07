43591367583467f68860489fe97b3304f1c9c1ae
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2024-06-07 10:40:49 +0200

    added css & imported components

--- Angelos-N/resources/js/app.js
+++ Angelos-N/resources/js/app.js
@@ -5,8 +5,44 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import angerloContact from './Components/angerloContact.vue';
+import angerloFooter from './Components/angerloFooter.vue';
+import angerloHeader from './Components/angerloHeader.vue';
+import angerloHomePage from './Components/angerloHomePage.vue';
+import angerloInloggen from './Components/angerloInloggen.vue';
+import angerloLatestEditions from './Components/angerloLatestEditions.vue';
+import angerloLatestNews from './Components/angerloLatestNews.vue';
+import angerloPdf from './Components/angerloPdf.vue';
+import backButton from './Components/backButton.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+Vue.component('angerlo-contact', 
+require('./Components/angerloContact.vue').default);
+
+Vue.component('angerlo-footer', 
+require('./Components/angerloFooter.vue').default);
+
+Vue.component('angerlo-header', 
+require('./Components/angerloHeader.vue').default);
+
+Vue.component('angerlo-homepage', 
+require('./Components/angerloHomePage.vue').default);
+
+Vue.component('angerlo-inloggen', 
+require('./Components/angerloInloggen.vue').default);
+
+Vue.component('angerlo-latest-editions', 
+require('./Components/angerloLatestEditions.vue').default);
+
+Vue.component('angerlo-latest-news', 
+require('./Components/angerloLatestNews.vue').default);
+
+Vue.component('angerlo-pdf', 
+require('./Components/angerloPdf.vue').default);
+
+Vue.component('back-button', 
+require('./Components/backButton.vue').default);
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
